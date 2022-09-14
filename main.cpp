9b47d2ce20539d44757ef5b4389ffba944fb3a0f
Benedek <pinter.benedek@hallgato.sze.hu>
2022-09-14 14:36:06 +0200

    Ezek a hibak vannak

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int* b = new int[NELEMENTS]; //hibas nev
+    std::cout << '1-100 ertekek duplazasa' //nincs idezojel es pontosvesszo 
+        for (int i = 0;) //hianyos for ciklus
+        {
+            b[i] = i * 2;
+        }
+    for (int i = 0; i; i++) //hianyos ciklus
     {
-        b[i] = i * 2;
+        std::cout << "Ertek:" //nincs pontosvesszo
     }
-    for (int i = 0; i; i++)
-    {
-        std::cout << "Ertek:"
-    }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
     for (int i = 0; i < N_ELEMENTS, i++)
     {
-        atlag += b[i]
+        atlag += b[i] //nincs pontosvesszo
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
