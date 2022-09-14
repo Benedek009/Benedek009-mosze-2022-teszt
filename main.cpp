6802127e32be9dca16682c1b765e2fffa331e461
Benedek <pinter.benedek@hallgato.sze.hu>
2022-09-14 14:40:46 +0200

    Javitva minden

--- main.cpp
+++ main.cpp
@@ -4,21 +4,22 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int* b = new int[NELEMENTS]; //hibas nev
-    std::cout << '1-100 ertekek duplazasa' //nincs idezojel es pontosvesszo 
-        for (int i = 0;) //hianyos for ciklus
-        {
-            b[i] = i * 2;
-        }
-    for (int i = 0; i; i++) //hianyos ciklus
+    int* b = new int[N_ELEMENTS]; //hibas nev
+    std::cout << "1-100 ertekek duplazasa"; //nincs idezojel es pontosvesszo 
+    for (int i = 0; i < N_ELEMENTS; i++) //hianyos for ciklus
     {
-        std::cout << "Ertek:" //nincs pontosvesszo
+        b[i] = (i + 1) * 2;
+    }
+    std::cout << "Ertek: " << std::endl;
+    for (int i = 0; i < N_ELEMENTS; i++) //hianyos ciklus
+    {
+        std::cout << b[i] << ", "; //nincs pontosvesszo
     }
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        atlag += b[i] //nincs pontosvesszo
+        atlag += b[i]; //nincs pontosvesszo
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
